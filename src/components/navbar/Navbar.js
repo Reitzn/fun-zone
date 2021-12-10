60a4200e907a4766760131f6ae3da879acbe60ce
Nick Reitz <Reitznia@gmail.com>
2021-12-10 18:40:30 -0500

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -21,17 +21,17 @@ export default function Header() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">
+              <Nav.Link eventKey="1" as={Link} to="/">
                 Home
               </Nav.Link>
-              <Nav.Link as={Link} to="/resume">
+              <Nav.Link eventKey="2" as={Link} to="/resume">
                 Resume
               </Nav.Link>
-              <Nav.Link as={Link} to="/contact">
+              <Nav.Link eventKey="3" as={Link} to="/contact">
                 Contact Me
               </Nav.Link>
               {currentUser ? (
-                <Nav.Link as={Link} to="/profile">
+                <Nav.Link eventKey="4" as={Link} to="/profile">
                   Profile
                 </Nav.Link>
               ) : null}
