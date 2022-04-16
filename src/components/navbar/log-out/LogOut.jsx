90b9b4122102f947b755a0a51d1d3555fb09294f
Nick Reitz <Reitznia@gmail.com>
2022-04-15 22:45:34 -0400

    removing unused imports

--- src/components/navbar/log-out/LogOut.jsx
+++ src/components/navbar/log-out/LogOut.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
 import { logout } from "../../../firebase";
 import { Link } from "react-router-dom";
